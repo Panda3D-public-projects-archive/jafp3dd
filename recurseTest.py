dba008b87215ca1f45bfd84b66e0ca455dc54782
supus71@gmail.com <supus71@gmail.com>
2011-12-24 00:49:59 +0000

    work on new tree class. remove the redundant path traversal. removed the always split criteria. Went to a "branch" model where branch nodes define the segments of a branch. Any branch can start on any node of another brach. This allows for trees with a primary trunk...like most trees. This also allows for each branch to "grow" unique from any sibling branch. The fractal tree had way too much symmetry for my liking.

--- recurseTest.py
+++ recurseTest.py
@@ -4,30 +4,52 @@ Created on Wed Dec 21 09:47:48 2011
 
 @author: shawn
 """
+import random
+from panda3d.core import Vec3, Quat
 
-
-class Branch():
-    def __init__(self,l=0,ang1=0,ang2=0,clist=[]):
-        self.length = l
-        self.theta = ang1
-        self.phi = ang2
-        self.children = clist
+class BranchNode():
+    def __init__(self,l=1,rad=1,Q=Quat(),pos=Vec3(0,0,0)):
+        self.length = l            # MOVE TO GENERATION ROUTINE. DON'T NEED LENGTHS, ONLY POSITIONS
+        self.radius = rad            # radius of child end (parent end will obvious have it's radius)
+        self.quat = Q         # orientation quaternion; which way am I pointing?
+        self.pos = pos     # center point of this branch node
+        self.children = []         # list of children from THIS branch's end
+        self.gen = 0               # generation ID
+#        
+    
+def getChildren(numSib=2,gen=1):
+    print gen
+    children = []
+    if gen < 4:
+        for i in range(numSib):
+            sib = BranchNode()
+            sib.children = getChildren(numSib,gen+1)
+            sib.gen = gen
+            children.append(sib)
+        return children
+    else:
+        return []
         
-def getChildren(numChil=2):
-    return [Branch()]*numChil
-    
-    
-    
     # parent is a list of N generations  
-    # each generation will split into numChildren
+    # each generation will split into numSibdren
     # children of generation n+1
 
+def selectGen(gen,Tree):
+    for child in Tree:
+        if child.gen == gen: print child.length
+        
 if __name__ == '__main__':
-    numChildren = 2
-    B = Branch(1,0,0,[])
-    B.children = getChildren()
-    for sib in B.children:
-        sib.children = getChildren()
+    numGens = 3    
+#    T = BranchNode() #1st gen (trunk)
+#    T.children = getChildren() #2nd gen
+    thisBranch = [BranchNode()] # make a starting node flat at 0,0,0
+    for i in range(numGens):
+        newNode = BranchNode()
+        newNode.pos += newNode.quat.getUp() * (1/(i+1))
+        newNode.quat.setHpr((0,0,15))
+        newNode.radius = 1/(i+1)
+        thisBranch.append(newNode)
+ 
         
     
         
