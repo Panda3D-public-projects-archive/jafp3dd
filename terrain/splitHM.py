804c4b6c065439e76a55b56b0517bb3eb8fa8fa4
supus71@gmail.com <supus71@gmail.com>
2012-01-17 03:08:46 +0000

    more clean up, some hacking for tiles as nodepaths; need to get AVNP and Camera NP as NPs under current Tile....seems like terraMgr might need getElev back to keep changes minor for now

--- terrain/splitHM.py
+++ terrain/splitHM.py
@@ -7,25 +7,31 @@ Created on Fri Dec 02 12:58:39 2011
 
 from PIL import Image
 import sys
+from math import ceil
 
-imageFile = sys.argv[1]
-ndiv = int(sys.argv[2])
-rootName = imageFile[:-3]
-sfx = imageFile[-3:]
+def splitToTiles(imageFile,Ltile):
+    rootName = imageFile[:-3]
+    sfx = imageFile[-3:]
+    
+    img = Image.open(imageFile)
+    img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    (Lx,Ly) = img.size
+#    divX = Lx / ndiv
+    nXdiv = ceil(Lx/Ltile)
+#    divY = Ly / ndiv
+    nYdiv = ceil(Ly/Ltile)
+#    print divX,divY
+    
+    for nx in range(nXdiv):
+        for ny in range(nYdiv):    
+#            box = (nx*divX,ny*divY,(nx+1)*divX+1,(ny+1)*divY+1) # +1 on end points to overlap tiles
+            box = (nx*Ltile,ny*Ltile,(nx+1)*Ltile+1,(ny+1)*Ltile+1) # +1 on end points to overlap tiles
+            subImg = img.crop(box).transpose(Image.FLIP_TOP_BOTTOM)
+            print subImg.size
+            subImg.save(rootName+"x%dy%d.%s"%(nx,ny,sfx))
+    
 
-img = Image.open(imageFile)
-img = img.transpose(Image.FLIP_TOP_BOTTOM)
-(Lx,Ly) = img.size
-divX = Lx / ndiv
-divY = Ly / ndiv
-print divX,divY
-
-for nx in range(ndiv):
-    for ny in range(ndiv):
-
-        box = (nx*divX,ny*divY,(nx+1)*divX+1,(ny+1)*divY+1) # +1 on end points to overlap tiles
-        subImg = img.crop(box).transpose(Image.FLIP_TOP_BOTTOM)
-        print subImg.size
-        subImg.save(rootName+"x%dy%d.%s"%(nx,ny,sfx))
-
-
+if __name__ == '__main__':
+    imageFile = sys.argv[1]
+    Ltile = int(sys.argv[2])
+    splitToTiles(imageFile,Ltile)
