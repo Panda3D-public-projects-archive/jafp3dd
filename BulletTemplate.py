bf332de1e50bc23bf2ded1cb4fc06ee45aafc1a0
supus71@gmail.com <supus71@gmail.com>
2012-06-13 19:10:51 +0000

    extracted camera controls and update as separate class

---
+++ BulletTemplate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 07 16:44:38 2012
+
+@author: us997259
+"""
+import direct.directbase.DirectStart
+from panda3d.core import Vec3
+from panda3d.bullet import BulletWorld, BulletPlaneShape, BulletRigidBodyNode, BulletBoxShape
+
